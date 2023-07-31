7d3bcc42eb981590e5dbf293bc04235cd49e05c2
Arafat Kabir Khan <arafatkabir@iut-dhaka.edu>
2023-07-31 23:57:59 +0600

    week8 day 4
    monday

---
+++ Week8/Day 4/notepad.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+    }
+    return 0;
+}
